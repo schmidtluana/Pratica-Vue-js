5c6d69ee57e03fb9b0dcf298319902fef4c86f8c
schmidtluana <schmidtluana54@gmail.com>
2020-03-29 15:58:53 -0300

    (alterações)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <caixas :caixinhas= "caixinhas"/>
+  <ferramentas :caixinhas= "caixinhas" @alteraCor="alteraCor($event)" @alterouTexto="alteraTexto($event)"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Caixas from "./components/Caixas.vue"
+import Ferramentas from "./components/Ferramentas.vue"
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Caixas,Ferramentas    
+  }, data:function () { //* função que retorna objeto//
+    return{
+      caixinhas: [{
+        titulo: "1",
+        cor: "background: red"
+      }, {
+        titulo: "2",
+        cor: "background: blue"
+      }, {
+        titulo: "3",
+        cor: "background: orange"
+      }]
+    }
+    }, methods:{
+      alteraCor: function (event){
+        this.caixinhas[event.caixa].cor = event.cor
+      }, alteraTexto: function(event){
+        this.caixinhas[event.caixa].titulo = event.titulo     }
+    }
   }
-}
+    
+  
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Ferramentas.vue
@@ -0,0 +1,75 @@
+<template>
+<div id="caixa2">
+<div>
+
+  <select v-model="caixaSel">
+      <option value="" selected disabled>Selecione uma caixa</option> <!-- selecionado debilitado -->
+      <option v-for="(caixa, index) in caixinhas" :key="index" :value="index">
+          {{ caixa.titulo }}
+    </option> <!-- seleciona a caixa pelo index -->
+  </select> 
+  <div id="paleta">
+      <div class="cor" :style="cor" v-for="(cor, index) in cores" :key='index' @click="alteraCor(cor)">
+
+      </div>
+  </div>
+  <input type="text" placeholder="Digite o nome da caixa" v-model="titulo">
+  <button @click="alterarTexto">OK</button>
+</div>
+</div>
+</template>
+
+div#
+<script>
+export default {
+    name: "Ferramentas",
+    props: ['caixinhas'], /* puxa la de caxinhas*/
+    data: function(){
+        return{
+            cores: ['background: pink', 'background: yellow', 'background: green'], /* vetor paleta de cores*/
+            caixaSel: '',
+            titulo:''
+        }
+    }, methods:{
+        alteraCor: function(cor){
+            this.$emit('alteraCor', {
+                cor: cor,
+                caixa: this.caixaSel
+            })
+
+            },alterarTexto: function(){
+            this.$emit('alterouTexto', {
+                titulo: this.titulo,
+                caixa: this.caixaSel
+            })
+
+        }
+    }
+}
+</script>
+
+<style>
+#caixa2{
+    border: 1px solid #ffffff;
+    width: 350px;
+    height: 160px;
+    margin: 30px auto 10px auto;
+    display: flex;
+    flex-wrap: wrap;
+}
+#paleta{
+    width: 300px;
+    height: 150px;
+    display: flex;
+
+}
+.cor{
+    width: 60px;
+    height: 60px;
+    border: 1px solid #ffffff;
+    margin-top: 6px;
+    margin-left: 7px;
+    
+}
+
+</style>
